37180517583288d432d83f8859579d251f401bd2
silvadiego-08 <djsilvacubas@uamv.edu.ni>
2025-04-08 10:48:01 -0600

    Semana del 7 al 11 de Abril

---
+++ Sesion1/ejercicio1.py
@@ -0,0 +1,13 @@
+#Declarar variables
+nombre_completo = "Diego Silva"
+edad = 16
+altura = 1.78
+soltero = True
+
+print(f"Nombre completo: {nombre_completo}")
+print(f"Edad: {edad}")
+print(f"Tipo de dato: {type(edad)}")
+print(f"Altura: {altura}")
+print(f"Tipo de dato: {type(altura)}")
+print(f"Estado Civil: {soltero}")
+print(f"Tipo de dato: {type(soltero)}")
---
+++ Sesion1/ejercicio2.py
@@ -0,0 +1,12 @@
+#Tipos de datos numerico
+
+cantidad = 10
+precio = 130
+total = cantidad * precio
+
+def ftexto(texto):
+    return f"{texto:>6}"
+
+print(f"Cantidad: {cantidad:>6}")
+print(f"Precio: {precio:>6}")
+print(f"Total: {total:>6}")
