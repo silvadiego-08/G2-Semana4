f78879d52cd74ffe5f22c91f1beffc8316f52333
silvadiego-08 <djsilvacubas@uamv.edu.ni>
2025-04-08 10:55:50 -0600

    Semana del 7 al 11 de Abril

---
+++ Sesion_1/Ejercicio1.py
@@ -0,0 +1,12 @@
+#Declarar las variables
+nombre_completo = "Diego Silva"
+edad = 16
+altura = 1.78
+soltero = True
+
+print(f"Nombre completo: {nombre_completo}")
+print(f"Tipo de dato: {type(nombre_completo)}")
+
+print(f"{edad} {type(edad)}")
+print(f"{altura:.2f} {type(altura)}")
+print(f"{soltero} {type(soltero)}")
